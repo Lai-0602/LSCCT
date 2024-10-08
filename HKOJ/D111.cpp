1e402472fdb4b2dfd5dc7438dd4a213a020f007e
Lai-0602 <lai0602@hotmail.com>
2024-10-08 22:27:54 +0800

    c

---
+++ HKOJ/D111.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+signed main() {
+    double w, h;
+    cin >> w >> h;
+    double bmi = w / pow(h, 2);
+    cout << setprecision(3) << bmi;
+    if (bmi < 18.5) {
+        cout << "Underweight\n";
+    } else if (bmi < 23) {
+        cout << "Normal\n";
+    } else if (bmi < 25) {
+        cout << "Overweight\n";
+    } else {
+        cout << "Obese\n";
+    }
+    return 0;
+}
